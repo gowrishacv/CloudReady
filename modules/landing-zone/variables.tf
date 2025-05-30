956653d735bd6c2f982cdd4f55d5c78a4d58edea
“Gowrisha <gowrish@outlook.com>
2025-05-30 18:51:31 +0200

    Add initial Terraform module for landing zone with resource group, VNet, and subnet configurations

---
+++ modules/landing-zone/variables.tf
@@ -0,0 +1,30 @@
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Virtual Network name"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for the VNet"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_prefixes" {
+  description = "Subnet address prefixes"
+  type        = list(string)
+}
